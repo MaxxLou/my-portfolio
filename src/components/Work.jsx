61e0ccc16eb2698a2419ab31bbd0553b93a2a7f5
MaxxLou <82404067+MaxxLou@users.noreply.github.com>
2024-05-11 18:33:39 +0300

    Update information

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,16 +2,23 @@ import WorkItem from "./WorkItem"
 
 const data = [
   {
+    year: 2024,
+    title: "Frontend Developer, freelance",
+    duration: "Present",
+    details:
+      "Create functional interfaces; Implement my own ideas in pet projects;    Expand my knowledge, applying it in practice.",
+  },
+  {
     year: 2020,
-    title: "UX/UI web designer, TABLETKI.UA",
+    title: "UX/UI Designer / Product Designer, TABLETKI.UA",
     duration: "3 years",
     details:
       "Created static and animated advertising creatives, conducted A/B testing; developed landing pages using the Wix website builder; redesigned and created UI Kits; designed presentations.",
   },
   {
-    year: 2018,
+    year: 2015,
     title: "Graphic designer, freelance",
-    duration: "2 years",
+    duration: "5 years",
     details:
       "Designed business cards, informational posters for patients at a private clinic, music festival posters; drew vector images for various websites; created collages from photographs.",
   },
